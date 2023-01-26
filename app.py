254eab9c8232b3ca514aab3936a47ed7617c0e07
JonasKnaup <99736881+JonasKnaup@users.noreply.github.com>
2023-01-26 10:26:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,9 @@ def predict():
         # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
         clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
+    except KeyError:
+        LOG.info("Key error")
+        return "Model not loaded"
     except Exception:
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
